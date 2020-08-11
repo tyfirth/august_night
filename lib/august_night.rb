ea9488879d05880413a6896cdd7285a5a79fea9b
tyfirth <tyfirth@gmail.com>
2020-08-11 15:18:57 -0700

    initial setup

---
+++ lib/august_night.rb
@@ -0,0 +1,6 @@
+require "august_night/version"
+
+module AugustNight
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/august_night/version.rb
@@ -0,0 +1,3 @@
+module AugustNight
+  VERSION = "0.1.0"
+end
